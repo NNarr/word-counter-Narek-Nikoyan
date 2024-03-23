30effbb120dd30d869c6229f2e8faa717f3daf22
NNarr <67961417+NNarr@users.noreply.github.com>
2024-03-23 15:26:06 +0400

    work.js

--- work.js
+++ work.js
@@ -1,1 +1,10 @@
+function analyzeText() {
+  const text = document.getElementById('inputText').value;
+  const letterCount = text.replace(/[^a-zA-Z]/g, '').length;
+  const wordCount = text.split(/\s+/).filter(word => word.length > 0).length;
+  const sentenceCount = text.split(/[.!?]+/).filter(sentence => sentence.length > 0).length;
 
+  document.getElementById('letterCount').textContent = letterCount;
+  document.getElementById('wordCount').textContent = wordCount;
+  document.getElementById('sentenceCount').textContent = sentenceCount;
+}
